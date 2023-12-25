8c609df1622e96c8926741d4c0cb8bc38f355c96
ashik-gf <bluenightskin1013@gmail.com>
2023-12-25 23:51:05 +0600

    uplode mask all components

---
+++ src/components/Elements/Mask/Mask.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SquirealMask from './SquirealMask'
+import ReuleauxTriangleMask from './ReuleauxTriangleMask'
+
+
+const Mask = () => {
+  return (
+    <div>
+     <SquirealMask />
+     <ReuleauxTriangleMask />
+    </div>
+  )
+}
+
+export default Mask
